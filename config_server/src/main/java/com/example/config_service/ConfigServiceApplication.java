ca127f823709ba46fd3791b25c1a102863c55814
Yu Jianlong <2063217709@qq.com>
2023-06-13 10:09:59 +0800

    add config server and trace

--- config_server/src/main/java/com/example/config_service/ConfigServiceApplication.java
+++ config_server/src/main/java/com/example/config_service/ConfigServiceApplication.java
@@ -2,14 +2,15 @@ package com.example.config_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-
+@EnableDiscoveryClient
 @EnableConfigServer
 @SpringBootApplication
-public class UtilsServiceApplication {
+public class ConfigServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UtilsServiceApplication.class, args);
+        SpringApplication.run(ConfigServiceApplication.class, args);
     }
 
 }
--- user_service/src/main/java/com/example/user_server/controller/UserController.java
+++ user_service/src/main/java/com/example/user_server/controller/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Logger;
 
 @RestController
 public class UserController {
